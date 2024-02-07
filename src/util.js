013cf97662298bd735d830c9e61be950ce0f02a0
Theophilus Nenhanga <theonenhanga@gmail.com>
2024-02-07 13:26:36 -0600

    added media queries

--- src/util.js
+++ src/util.js
@@ -1,8 +1,9 @@
 export function checkHighScores(messages){
+    // delete duplicate wordle scores
+
     messages.sort((a, b) => {
-        const aValue = a.message === "X"  ||  a.message === "x" ? -Infinity : parseInt(a.message);
-        const bValue = b.message === "X"  ||  b.message === "x" ? -Infinity : parseInt(b.message);
+        const aValue = a.message === "X"  ||  a.message === "x" ? Infinity : parseInt(a.message);
+        const bValue = b.message === "X"  ||  b.message === "x" ? Infinity : parseInt(b.message);
         return aValue - bValue;
     });
-    console.log(messages);
 }
